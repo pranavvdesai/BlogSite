ad7e7aae73392f71c17408928547d4d884a2be95
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-12 16:36:18 +0530

    show username with post

--- controllers/showpost.js
+++ controllers/showpost.js
@@ -1,11 +1,23 @@
-var Post = require('../models/post')
+var Post = require("../models/post");
 
-module.exports.showPost = (req,res)=>{
-    Post.find({},(err,posts)=>{
-        if(err){
-            console.log('err in showing');
-            return;
-        }
-        return res.render('home.ejs',posts=posts)
-    })
-}
+// module.exports.showPost = (req,res)=>{
+//     Post.find({},(err,posts)=>{
+//         if(err){
+//             console.log('err in showing');
+//             return;
+//         }
+//         return res.render('home',{posts:posts})
+//     })
+// }
+
+module.exports.showPost = (req, res) => {
+  Post.find({})
+    .populate("user")
+    .exec((err, posts) => {
+      if (err) {
+        console.log("err in showing");
+        return;
+      }
+      return res.render("home", { posts: posts });
+    });
+};
