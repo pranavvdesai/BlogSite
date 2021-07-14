c64fea3539c45f8e1a78a728942e55373e3b20c3
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-15 01:24:20 +0530

    created routes for comments

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,11 @@ const passport = require('passport')
 
 const usersController = require('../controllers/users_controller')
 const postController = require('../controllers/post_controller')
-
+const commentController = require('../controllers/comment_controller')
 
 
 router.post('/postcreate',passport.checkAuthentication,postController.createPost)
+router.post('/commentcreate',passport.checkAuthentication,commentController.createComment)
 
 
 router.get('/profile',passport.checkAuthentication,usersController.profile)
