da81be267d3f4648713d2d49224d3e3075de8e06
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-15 01:23:41 +0530

    post model update ref

--- models/post.js
+++ models/post.js
@@ -14,7 +14,7 @@ const postSchema = new mongoose.Schema(
     comments: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Comments'
+        ref: 'Comment'
       }
     ]
   },
