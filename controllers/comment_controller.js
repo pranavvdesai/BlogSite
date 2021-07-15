33d5d2a4b4d2cba2bdbdd86432c364c9d651611b
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-15 23:15:45 +0530

    destroy comments

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,12 +1,39 @@
-var Comment = require('../models/comments')
+const Comment = require('../models/comments')
+const Post = require('../models/post')
 
-
+// check if the post actually exist to which the comment has been given, as it can be hampered by inspect elemnet
 module.exports.createComment = (req,res)=>{
-    Comment.create({content: req.body.content,user: req.user._id,post: req.post._id},(err,post)=>{
-        if (err) {
-            console.log('err in creating comment');
-            return;
-          }
-        return res.redirect('back')
+    Post.findById(req.body.post,(err,post)=>{
+        if(post)
+        {
+            Comment.create({content: req.body.content,user: req.user._id,post: req.body.post},(err,comment)=>{
+                if (err) {
+                    console.log('err in creating comment');
+                    return;
+                  }
+                post.comments.push(comment)
+                post.save()
+                
+                return res.redirect('back')
+            })
+        }
+    })
+}
+
+module.exports.deletecomment = (req,res)=>{       
+    Comment.findById(req.params.id,(err,comment)=>{
+        if(comment.user == req.user.id || comment.post.user == req.user.id){
+            let postId = comment.post
+
+            comment.remove()
+
+            Post.findByIdAndUpdate(postId,{$pull: {comments: req.params.id}},(err,post)=>{
+                
+                return res.redirect('back')
+            })
+        }
+        else{
+            return res.redirect('back')
+        }
     })
 }
