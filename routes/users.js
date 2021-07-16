f40b99c6b0963f67edf86ee760f59c513da94e99
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-16 17:39:32 +0530

    routes for update profile

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,16 @@ const passport = require('passport')
 const usersController = require('../controllers/users_controller')
 const postController = require('../controllers/post_controller')
 const commentController = require('../controllers/comment_controller')
+const profileController = require('../controllers/profile_controller')
+const updateController = require('../controllers/update_profile')
 
 
 router.post('/postcreate',passport.checkAuthentication,postController.createPost)
 router.post('/commentcreate',passport.checkAuthentication,commentController.createComment)
 router.get('/delete/:id',passport.checkAuthentication,postController.destroy)
 router.get('/deletecomment/:id',passport.checkAuthentication,commentController.deletecomment)
+router.get('/profile/:id',passport.checkAuthentication,profileController.profile)
+router.post('/updateprofile/:id',passport.checkAuthentication,profileController.update)
 
 
 router.get('/profile',passport.checkAuthentication,usersController.profile)
