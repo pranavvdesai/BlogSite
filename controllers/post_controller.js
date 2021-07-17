b5d4e1e516c69b341f4b82ae6a6977f82f51d3ab
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-17 23:33:30 +0530

    prettier

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,27 +1,29 @@
-var Post = require('../models/post')
-const Comment = require('../models/comments')
+var Post = require("../models/post");
+const Comment = require("../models/comments");
 
-module.exports.createPost = (req,res)=>{
-    Post.create({content: req.body.content,user: req.user._id},(err,post)=>{
-        if (err) {
-            console.log('err in creating');
-            return;
-          }
-        return res.redirect('back')
-    })
-}
+module.exports.createPost = (req, res) => {
+  Post.create(
+    { content: req.body.content, user: req.user._id },
+    (err, post) => {
+      if (err) {
+        console.log("err in creating");
+        return;
+      }
+      return res.redirect("back");
+    }
+  );
+};
 
-module.exports.destroy = (req,res)=>{
-    Post.findById(req.params.id,(err,post)=>{
-        // .id means converting the object id into string
-        if(post.user == req.user.id){
-            post.remove()
-            Comment.deleteMany({post: req.params.id},(err)=>{
-                return res.redirect('back')
-            })
-        }
-        else{
-            return res.redirect('back')
-        }
-    })
-}
+module.exports.destroy = (req, res) => {
+  Post.findById(req.params.id, (err, post) => {
+    // .id means converting the object id into string
+    if (post.user == req.user.id) {
+      post.remove();
+      Comment.deleteMany({ post: req.params.id }, (err) => {
+        return res.redirect("back");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
