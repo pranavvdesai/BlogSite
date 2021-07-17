fe709bd83461bc73cfcaf8f62502f29975829cb2
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-17 23:33:57 +0530

    prettier

--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -1,27 +1,25 @@
-const User = require("../models/user")
+const User = require("../models/user");
 
-module.exports.profile = (req,res)=>{
-    User.findById(req.params.id,(err,user)=>{
-        if (err) {
-            console.log('err in finding profile');
-            return;
-          }
-        return res.render('profile',{profile_user:user})
-    })
-
-}
-
-module.exports.update = (req,res)=>{
-    if(req.user.id == req.params.id)
-    {
-        User.findByIdAndUpdate(req.params.id,{name: req.body.profile_name,email: req.body.profile_email},(err,user)=>{
-            return res.redirect('back')
-
-        })
+module.exports.profile = (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err) {
+      console.log("err in finding profile");
+      return;
     }
-    else{
-        return res.status(401).send('unauthorised')
-    }
-   
+    return res.render("profile", { profile_user: user });
+  });
+};
 
-}
+module.exports.update = (req, res) => {
+  if (req.user.id == req.params.id) {
+    User.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.profile_name, email: req.body.profile_email },
+      (err, user) => {
+        return res.redirect("back");
+      }
+    );
+  } else {
+    return res.status(401).send("unauthorised");
+  }
+};
