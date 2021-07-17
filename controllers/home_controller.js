329034b4e9f85e0c7373f60964ca9f1b81d1892c
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-17 23:34:39 +0530

    prettier

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,44 +1,44 @@
-var User = require('../models/user');
+var User = require("../models/user");
 
 module.exports.register = (req, res) => {
   if (req.isAuthenticated()) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   // console.log(res)
   console.log(req.cookies);
-  res.cookie('gttg', 25);
-  return res.render('register.ejs', { title: 'register' });
+  res.cookie("gttg", 25);
+  return res.render("register.ejs", { title: "register" });
 };
 
 module.exports.login = (req, res) => {
   if (req.isAuthenticated()) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
-  return res.render('login.ejs', { title: 'login' });
+  return res.render("login.ejs", { title: "login" });
 };
 
 module.exports.create = (req, res) => {
   console.log(req.body);
   if (req.body.password != req.body.confirmpassword) {
-    return res.redirect('back');
+    return res.redirect("back");
   }
 
   User.findOne({ email: req.body.email }, (err, user) => {
     if (err) {
-      console.log('error in finding user');
+      console.log("error in finding user");
       return;
     }
     if (!user) {
       User.create(req.body, (err, user) => {
         if (err) {
-          console.log('err in creating');
+          console.log("err in creating");
           return;
         }
 
-        return res.redirect('/login');
+        return res.redirect("/login");
       });
     } else {
-      return res.redirect('back');
+      return res.redirect("back");
     }
   });
 };
@@ -47,12 +47,12 @@ module.exports.loginvalidation = (req, res) => {
   // branch
 
   // using passport
-  return res.redirect('/');
+  return res.redirect("/");
 };
 
 module.exports.logout = (req, res) => {
   req.logout();
-  return res.redirect('/');
+  return res.redirect("/");
 };
 
 //bi
