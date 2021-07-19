cce665a58cc1b52faea7edf8cdf0b3c6206e3f20
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-19 23:40:50 +0530

    a flash messages

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -7,16 +7,17 @@ passport.use(
   new LocalStrategy(
     {
       usernameField: 'email',
+      passReqToCallback: true
     },
-    function (email, password, done) {
+    function (req,email, password, done) {
       User.findOne({ email: email }, (err, user) => {
         if (err) {
-          console.log('error in finding user --> passport');
+          req.flash('error', err);
           return done(err);
         }
 
         if (!user || user.password != password) {
-          console.log('invalid username/password');
+          req.flash('error', 'Invalid email or password');
           return done(null, false);
         }
 
