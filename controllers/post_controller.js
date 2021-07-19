9d460de3f7fbadad0082cdd2b3128aa4b5b0c1a1
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-19 23:41:57 +0530

    a flash messages

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -17,9 +17,10 @@ const Comment = require("../models/comments");
 module.exports.createPost = async (req, res) => {
   try {
     await Post.create({ content: req.body.content, user: req.user._id });
+    req.flash('success',"Post Created");
     return res.redirect("back");
   } catch (err) {
-    console.log(err);
+    req.flash('error',err);
     return;
   }
 };
@@ -45,12 +46,14 @@ module.exports.destroy = async (req, res) => {
     if (post.user == req.user.id) {
       post.remove();
       await Comment.deleteMany({ post: req.params.id });
+      req.flash('success',"Post Removed");
       return res.redirect("back");
     } else {
+      req.flash('error',"You can't delete this post");
       return res.redirect("back");
     }
   } catch (err) {
-    console.log(err);
+    req.flash('error',err);
     return;
   }
 };
