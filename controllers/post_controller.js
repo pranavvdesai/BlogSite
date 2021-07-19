109ee308a113b49e215f907a7d99ddacb0762597
pranavvdesai <pranavdesai.psd@gmail.com>
2021-07-19 16:39:42 +0530

    chenged to async

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,29 +1,56 @@
 var Post = require("../models/post");
 const Comment = require("../models/comments");
 
-module.exports.createPost = (req, res) => {
-  Post.create(
-    { content: req.body.content, user: req.user._id },
-    (err, post) => {
-      if (err) {
-        console.log("err in creating");
-        return;
-      }
-      return res.redirect("back");
-    }
-  );
+// module.exports.createPost = (req, res) => {
+//   Post.create(
+//     { content: req.body.content, user: req.user._id },
+//     (err, post) => {
+//       if (err) {
+//         console.log("err in creating");
+//         return;
+//       }
+//       return res.redirect("back");
+//     }
+//   );
+// };
+
+module.exports.createPost = async (req, res) => {
+  try {
+    await Post.create({ content: req.body.content, user: req.user._id });
+    return res.redirect("back");
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
 
-module.exports.destroy = (req, res) => {
-  Post.findById(req.params.id, (err, post) => {
+// module.exports.destroy = (req, res) => {
+//   Post.findById(req.params.id, (err, post) => {
+//     // .id means converting the object id into string
+//     if (post.user == req.user.id) {
+//       post.remove();
+//       Comment.deleteMany({ post: req.params.id }, (err) => {
+//         return res.redirect("back");
+//       });
+//     } else {
+//       return res.redirect("back");
+//     }
+//   });
+// };
+
+module.exports.destroy = async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
     // .id means converting the object id into string
     if (post.user == req.user.id) {
       post.remove();
-      Comment.deleteMany({ post: req.params.id }, (err) => {
-        return res.redirect("back");
-      });
+      await Comment.deleteMany({ post: req.params.id });
+      return res.redirect("back");
     } else {
       return res.redirect("back");
     }
-  });
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
