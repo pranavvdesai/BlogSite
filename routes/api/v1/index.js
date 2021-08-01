c15fad6c34199a1a8444f7403b0a4a80eeb87090
pranavvdesai <pranavdesai.psd@gmail.com>
2021-08-01 14:16:49 +0530

    routes api index

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/posts', require('./posts'));
+router.use('/users', require('./users'));
 
-
-module.exports = router;
+module.exports = router; 
