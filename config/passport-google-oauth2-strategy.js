70523636a9cb0142dd570efd614928f93eb9708d
pranavvdesai <pranavdesai.psd@gmail.com>
2021-08-09 17:40:58 +0530

    p.env

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -3,15 +3,15 @@ const passport = require("passport");
 const googleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const crypto = require("crypto");
 const User = require("../models/user");
+const env = require('./environment')
 
 // tell passport to use the google strategy
 passport.use(
   new googleStrategy(
     {
-      clientID:
-        "429877536839-5vvdb4ikjq04cc9a6ojoa5ntek6sb3rl.apps.googleusercontent.com",
-      clientSecret: "dvq33rumXRGwcSuxT7fJ3rhy",
-      callbackURL: "http://localhost:8000/auth/google/callback",
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     (accessToken, refreshToken, profile, done) => {
       // find the user in the database based on their google id
