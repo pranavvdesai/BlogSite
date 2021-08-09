fa15d075f1a9d92d9ae67ea0e29c1ebd32b80e53
pranavvdesai <pranavdesai.psd@gmail.com>
2021-08-09 17:40:20 +0530

    process.env

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -3,6 +3,8 @@
 const User = require("../../../models/user");
 const jwt = require("jsonwebtoken");
 
+const env = require('../../../config/environment')
+
 module.exports.loginvalidation = async (req, res) => {
   try {
     let user = await User.findOne({ email: req.body.email });
@@ -12,7 +14,7 @@ module.exports.loginvalidation = async (req, res) => {
     return res.json(200, {
       message: "sign in suxas, here is ur token, keep it safe",
       data: {
-        token: jwt.sign(user.toJSON(), "pronob", { expiresIn: "1000000" }),
+        token: jwt.sign(user.toJSON(), process.env.JWT_SECRET , { expiresIn: "1000000" }),
       },
     });
   } catch (err) {
