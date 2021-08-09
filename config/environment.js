5174ce561ba1fd0107def9e764b9c21219e8602a
pranavvdesai <pranavdesai.psd@gmail.com>
2021-08-10 01:53:18 +0530

    log + rfs

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,26 @@
 // // require('dotenv').config({path: '../.env'})
 
+const fs = require('fs')
+const path = require('path')
+const rfs = require('rotating-file-stream')
 
-// const development = {
+// variable which defines where the log will be stored
+const logDirectory = path.join(__dirname, '../production_logs')
+// check if the directory exists, if not make one
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+const accessLogStream = rfs.createStream('access.log', {
+    interval : '1d',
+    path     : logDirectory
+})
+// if(process.env.NODE_ENV === 'development'){
+//     morgan: {
+//         mode: 'dev',
+//         options: {stream: accessLogStream}
+//     }
+// }
+
+const development = {
 //   name: "development",
 //   asset_path: "./assets",
 //   session_cookie_key: "choot",
@@ -21,9 +40,14 @@
 //   clientSecret: "dvq33rumXRGwcSuxT7fJ3rhy",
 //   callbackURL: "http://localhost:8000/auth/google/callback",
 //   jwt_secret: "pronob",
-// };
+     morgan: {
+         mode: 'dev',
+         options: {stream: accessLogStream}
+     }
 
-// const production = {
+};
+
+const production = {
 //   name: "production",
 //   asset_path: process.env.ASSET_PATH,
 //   session_cookie_key: process.env.SESSION_COOKIE_KEY,
@@ -42,6 +66,10 @@
 //   clientSecret: process.env.CLIENT_SECRET,
 //   callbackURL: process.env.GOOGLE_CALLBACK_URL,
 //   jwt_secret: process.env.JWT_SECRET,
-// };
+    morgan: {
+        mode: 'combined',
+        options: {stream: accessLogStream}
+    }
+};
 
-// module.exports = eval(process.env.NODE_ENV) == undefined ? development : eval(process.env.NODE_ENV);
+module.exports = eval(process.env.NODE_ENV) == undefined ? development : eval(process.env.NODE_ENV);
